88a4d741d9eab36869902736d3aa48ce1f5d08bb
B0Dim <bodim@me.com>
2021-12-26 17:58:42 +0300

    create ui

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var countRedColor: UILabel!
+    @IBOutlet weak var countGreenColor: UILabel!
+    @IBOutlet weak var countBlueColor: UILabel!
+    
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    private func changeViewColor() {
+        
+    }
 
-
+    @IBAction func changerRedValue(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeGreenValue(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeBlueValue(_ sender: UISlider) {
+    }
+    
 }
 
